cb3c1c9d25f2a8b60b2d2126d8e6f8752d440d5d
alanio <alaniocarvalho.cs@gmail.com>
2016-11-23 14:31:52 -0300

    Add django admin bootstrapped

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,3 +5,9 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('blog.urls')),
 ]
+
+admin.site.site_header = 'BLOG'
+admin.site.index_title = 'BLOG ADMIN'
+admin.site.site_title = 'Admin Blog'
+
+
